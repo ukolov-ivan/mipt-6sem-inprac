d4bdeeafea90c045113ae1d869531073dec77092
Ukolov Ivan <ukolov.i@phystech.edu>
2024-10-13 18:51:18 +0300

    Fixed publish workflow

---
+++ backend/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for PostgreSQL..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+        sleep 1.0
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+exec "$@"
--- frontend/src/features/projects/issueSlice.js
+++ frontend/src/features/projects/issueSlice.js
@@ -67,7 +67,7 @@ export const updateIssue = createAsyncThunk(
                 error.message ||
                 error.toString();
             return thunkAPI.rejectWithValue(message);
-        };
+        }
     }
 );
 
